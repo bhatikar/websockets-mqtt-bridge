f1aef0478635bdae7a821db7158f4e9096dd27cb
Abhijeet Bhatikar <Abhijeet Bhatikar>
2019-03-03 00:39:06 +0000

    Bridge with MQTT support

--- app.js
+++ app.js
@@ -2,34 +2,49 @@ var app = require('http').createServer()
   , io = require('socket.io').listen(app)
 
 app.listen(3000);
+const mqtt = require('mqtt')
 
 var clients = {};
 
 io.sockets.on('connection', function (socket) {
-  socket.on('bridge-connect', function(data){
-    clients[data.id] = {
-      'socket': socket.id,
-      'topic': data.topic
-    };
-	  console.log(clients)
+	socket.on('bridge-connect', function(data){
+		if(clients[data.id] !== undefined)
+		{
+			console.log('user exists')
+			socket.disconnect();
+		}
+		else
+		{
+			clients[data.id] = {
+				'socket': socket.id,
+				'topic': data.topic
+			};
+		}
+		console.log(clients)
   });
 
   //Received a publish
   socket.on('publish-message', function(data){
     console.log("Sending: " + data.content + " to " + data.topic);
+	  client.publish(data.topic, data.content);
 	  for(var name in clients) {
-		  console.log(name)
-		  if(clients[name].topic === data.topic) {
-			  console.log(clients[name].socket)
-			  console.log('found matching sockets topic')
+		  //console.log(name)
+		  //console.log(clients[name].topic)
+		  //console.log(data.topic)
+		  //console.log(clients[name].socket)
+		  //console.log(socket.id)
+		  if(clients[name].topic === data.topic && clients[name].socket !== socket.id) {
+			  //console.log(clients[name].socket)
+			  //console.log('found matching sockets topic')
 			  //Send to the subscribers
-			  io.sockets.connected[clients[name].socket].emit('subscribe-message', {"content":"reply", "topic": data.topic})
+			  io.sockets.connected[clients[name].socket].emit('subscribe-message', {"content": data.content, "topic": data.topic})
 		  }
-	  }	
+	  }
   });
 
   //Removing the socket on disconnect
   socket.on('disconnect', function() {
+	console.log('Disconnecting')
   	for(var name in clients) {
   		if(clients[name].socket === socket.id) {
   			delete clients[name];
@@ -39,5 +54,35 @@ io.sockets.on('connection', function (socket) {
   })
 });
 
+var options = {
+    port: 1883,
+    host: 'mqtt://localhost',
+    clientId: 'mqttws_bridge',
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
 
+const client = mqtt.connect('mqtt://localhost', options)
 
+client.on('connect', () => {
+    console.log('mqtt client connected');
+  client.subscribe('#')
+})
+
+client.on('message', (topic, message) => {
+	console.log('received message %s %s', topic, message)
+	console.log("MQTT to WS Sending: " + message + " to " + topic);
+	for(var name in clients) {
+		console.log(name)
+		if(clients[name].topic === topic) {
+			console.log(clients[name].socket)
+			console.log('found matching sockets topic')
+			//Send to the subscribers
+			io.sockets.connected[clients[name].socket].emit('subscribe-message', {"content": message, "topic": topic})
+		}
+	}
+  });
